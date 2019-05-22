1efb455448c2364a47efb6315aec6d0321b16ff4
John Michael Laceda <john.laceda@gmail.com>
2019-05-21 23:26:12 -0700

    more project setup

---
+++ models/burger.js
@@ -0,0 +1,9 @@
+const orm = require('../config/orm.js');
+
+let burger = {};
+
+// TODO: create the code that will call
+// the ORM functions using burger specific 
+// input for the ORM.
+
+module.exports = burger;
