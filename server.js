dcedcbe51c28da428dc73b91f4ac8ee201975a54
John Michael Laceda <john.laceda@gmail.com>
2019-05-24 15:32:23 -0700

    add handlebars and controller middleware

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require("express-handlebars");
 const morgan = require('morgan');
 const compression = require('compression');
 
@@ -6,8 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use(morgan('combined'));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(require('./controllers/burgers_controller'))
 
 app.listen(PORT);
