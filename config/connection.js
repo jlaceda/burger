4bebb5c6d06d8fd11564ecea6914fce7ac0dc942
John Michael Laceda <john.laceda@gmail.com>
2019-05-24 13:59:42 -0700

    use `.env` for mysql connection config

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,17 @@
+require('dotenv').config();
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
-	user: "root",
-	password: "",
-	database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: process.env.MYSQL_HOST,
+		port: process.env.MYSQL_PORT,
+		user: process.env.MYSQL_USER,
+		password: process.env.MYSQL_PASS,
+		database: "burgers_db"
+	});
+}
 
 module.exports = connection;
