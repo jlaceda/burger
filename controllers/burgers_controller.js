632394dc3555f57721fc71dc223166198049f4e5
John Michael Laceda <john.laceda@gmail.com>
2019-05-24 14:25:08 -0700

    implement burger controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,29 @@ const express = require('express');
 const burger = require('../models/burger.js');
 const router = express.Router();
 
-// TODO: create routes
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+router.get('/', async (req, res) => {
+	res.send(await burger.getAll());
+});
+
+router.post('/', async (req, res) => {
+	const result = await burger.addOne(req.body.burgerName);
+	if (result.warningCount > 0 || result.affectedRows !== 1) {
+		res.status(500).end();
+	}
+	res.status(201).send({
+		insertId: result.insertId
+	});
+})
+
+router.put('/:id', async (req, res) => {
+	const result = await burger.devourOne(req.params.id);
+	if (result.warningCount > 0 || result.affectedRows !== 1) {
+		res.status(500).end();
+	}
+	res.status(201).end();
+});
 
 module.exports = router;
