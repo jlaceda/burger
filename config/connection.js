32d2be48013e012f0996018284a09a2defc9ca51
John Michael Laceda <john.laceda@gmail.com>
2019-05-26 22:17:18 -0700

    not using jawsql

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,12 @@
 require('dotenv').config();
 const mysql = require('mysql');
-let connection;
 
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	connection = mysql.createConnection({
-		host: process.env.MYSQL_HOST,
-		port: process.env.MYSQL_PORT,
-		user: process.env.MYSQL_USER,
-		password: process.env.MYSQL_PASS,
-		database: "burgers_db"
-	});
-}
+const connection= mysql.createConnection({
+	host: process.env.MYSQL_HOST,
+	port: process.env.MYSQL_PORT,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASS,
+	database: "burgers_db"
+});
 
 module.exports = connection;
